235b3e4679c018dd250f692c4eabd287da11d6ee
ChangeSuger <camille_0315@buaa.edu.cn>
2024-10-12 22:58:51 +0800

    feat: 补充示例

--- src/config/sidebar.ts
+++ src/config/sidebar.ts
@@ -3,4 +3,6 @@ import type { SidebarItem } from 'node_modules/@astrojs/starlight/schemas/sideba
 export const sidebar: SidebarItem[] = [
   { label: '首页', link: '/', translations: {}, attrs: {} },
   { label: '第一个示例', link: '/examples/01-first-example/', translations: {}, attrs: {} },
+  { label: '容器', link: '/examples/02-container/', translations: {}, attrs: {} },
+  { label: '实战-电视机雪花特效', link: '/examples/03-tv-noise-sound/', translations: {}, attrs: {} },
 ];
