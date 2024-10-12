877cd1b30eac4bf24f8cf2e7528c1417feff1a8a
ChangeSuger <camille_0315@buaa.edu.cn>
2024-10-12 15:38:52 +0800

    feat: 模板基建

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "PixiJS 6 Examples",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      defaultLocale: "zh-CN",
+      locales: {
+        "zh-CN": {
+          label: "简体中文",
+          lang: "zh-CN",
+        },
+      },
+      sidebar: [],
+			customCss: ['./src/tailwind.css'],
+    }),
+    tailwind({ applyBaseStyles: false }),
+  ],
 });
---
+++ src/config/sidebar.ts
@@ -0,0 +1,6 @@
+import type { SidebarItem } from 'node_modules/@astrojs/starlight/schemas/sidebar';
+
+export const sidebar: SidebarItem[] = [
+  { label: '首页', link: '/', translations: {}, attrs: {} },
+  { label: '第一个示例', link: '/examples/01-first-example/', translations: {}, attrs: {} },
+];
