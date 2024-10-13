a2aefc6f56d269408f6ca33ce59d5a7ebd339e25
ChangeSuger <camille_0315@buaa.edu.cn>
2024-10-14 01:38:54 +0800

    feat: 更新示例

--- src/config/sidebar.ts
+++ src/config/sidebar.ts
@@ -4,5 +4,6 @@ export const sidebar: SidebarItem[] = [
   { label: '首页', link: '/', translations: {}, attrs: {} },
   { label: '第一个示例', link: '/examples/01-first-example/', translations: {}, attrs: {} },
   { label: '容器', link: '/examples/02-container/', translations: {}, attrs: {} },
-  { label: '实战-电视机雪花特效', link: '/examples/03-tv-noise-sound/', translations: {}, attrs: {} },
+  { label: '图形', link: '/examples/03-graphics/', translations: {}, attrs: {} },
+  { label: '实战-电视机雪花特效', link: '/examples/extra-tv-noise-sound/', translations: {}, attrs: {} },
 ];
