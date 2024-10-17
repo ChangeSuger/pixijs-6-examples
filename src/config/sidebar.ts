f067faccb6deca5813a954572de3783df23775fa
ChangeSuger <camille_0315@buaa.edu.cn>
2024-10-17 21:18:49 +0800

    style: 简化sidebar的设置方式

--- src/config/sidebar.ts
+++ src/config/sidebar.ts
@@ -1,9 +1,9 @@
 import type { SidebarItem } from 'node_modules/@astrojs/starlight/schemas/sidebar';
 
 export const sidebar: SidebarItem[] = [
-  { label: '首页', link: '/', translations: {}, attrs: {} },
-  { label: '第一个示例', link: '/examples/01-first-example/', translations: {}, attrs: {} },
-  { label: '容器', link: '/examples/02-container/', translations: {}, attrs: {} },
-  { label: '图形', link: '/examples/03-graphics/', translations: {}, attrs: {} },
-  { label: '实战-电视机雪花特效', link: '/examples/extra-tv-noise-sound/', translations: {}, attrs: {} },
-];
+  { label: '首页', link: '/' },
+  { label: '第一个示例', link: '/examples/01-first-example/' },
+  { label: '容器', link: '/examples/02-container/' },
+  { label: '图形', link: '/examples/03-graphics/' },
+  { label: '实战-电视机雪花特效', link: '/examples/extra-tv-noise-sound/' },
+].map(item => ({...item, translations: {}, attrs: {}}));
